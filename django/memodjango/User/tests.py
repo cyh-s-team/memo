086522ad12d3ff8424303f7e68149667a347dbbf
jumpshotsniper <1057152966@qq.com>
2022-06-05 15:14:46 +0800

    修改用户详情测试（后端）（git action自动测试）

--- django/memodjango/User/tests.py
+++ django/memodjango/User/tests.py
@@ -28,38 +28,38 @@ class UserTest(TestCase):
     #
 
     # # 用户注册单元测试
-    def test_UserRegister(self):
+    # def test_UserRegister(self):
+    #     headers = {"content-type": "application/json"}
+    #
+    #     # 成功案例
+    #     json_data1 = {'userid': '52488851', 'username': '656', 'passwd': '123'}
+    #     r1 = requests.post(self.UserRegister_url, json=json_data1, headers=headers)
+    #     result1 = r1.json()
+    #     self.assertEqual(result1['ret'], 0)
+    #
+    #     # 失败案例
+    #     json_data2 = {'userid': '9949388491', 'username': '', 'passwd': '123'}
+    #     r2 = requests.post(self.UserRegister_url, json=json_data2, headers=headers)
+    #     result2 = r2.json()
+    #     self.assertEqual(result2['ret'], 1)
+    #
+
+    #修改用户信息单元测试
+    def test_UserChange(self):
+
         headers = {"content-type": "application/json"}
 
-        # 成功案例
-        json_data1 = {'userid': '52488851', 'username': '656', 'passwd': '123'}
-        r1 = requests.post(self.UserRegister_url, json=json_data1, headers=headers)
+        #成功案例
+        json_data1 = {'userid': '1', 'username': 'cyhcyh', 'passwd': '123123'}
+        r1 = requests.post(self.UserChange_url, json=json_data1, headers=headers)
         result1 = r1.json()
         self.assertEqual(result1['ret'], 0)
 
         # 失败案例
-        json_data2 = {'userid': '9949388491', 'username': '', 'passwd': '123'}
-        r2 = requests.post(self.UserRegister_url, json=json_data2, headers=headers)
+        json_data2 = {'userid': '111', 'username': 'cyhcyh', 'passwd': '123123'}
+        r2 = requests.post(self.UserChange_url, json=json_data2, headers=headers)
         result2 = r2.json()
         self.assertEqual(result2['ret'], 1)
-    #
-
-    #修改用户信息单元测试
-    # def test_UserChange(self):
-    #
-    #     headers = {"content-type": "application/json"}
-    #
-    #     # 成功案例
-    #     # json_data1 = {'userid': '1', 'username': 'cyhcyh', 'passwd': '123123'}
-    #     # r1 = requests.post(self.UserChange_url, json=json_data1, headers=headers)
-    #     # result1 = r1.json()
-    #     # self.assertEqual(result1['ret'], 0)
-    #
-    #     # 失败案例
-    #     json_data2 = {'userid': '111', 'username': 'cyhcyh', 'passwd': '123123'}
-    #     r2 = requests.post(self.UserChange_url, json=json_data2, headers=headers)
-    #     result2 = r2.json()
-    #     self.assertEqual(result2['ret'], 1)
 
     #获取用户详情测试(用于找回密码)
     # def test_GetUser(self):
