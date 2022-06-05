b54c674c483df75971b05fc6f90dd8832476d686
jumpshotsniper <1057152966@qq.com>
2022-06-05 15:17:23 +0800

    查询用户详情测试（后端）（git action自动测试）

--- django/memodjango/User/tests.py
+++ django/memodjango/User/tests.py
@@ -45,33 +45,33 @@ class UserTest(TestCase):
     #
 
     #修改用户信息单元测试
-    def test_UserChange(self):
-
-        headers = {"content-type": "application/json"}
-
-        #成功案例
-        json_data1 = {'userid': '1', 'username': 'cyhcyh', 'passwd': '123123'}
-        r1 = requests.post(self.UserChange_url, json=json_data1, headers=headers)
-        result1 = r1.json()
-        self.assertEqual(result1['ret'], 0)
-
-        # 失败案例
-        json_data2 = {'userid': '111', 'username': 'cyhcyh', 'passwd': '123123'}
-        r2 = requests.post(self.UserChange_url, json=json_data2, headers=headers)
-        result2 = r2.json()
-        self.assertEqual(result2['ret'], 1)
-
-    #获取用户详情测试(用于找回密码)
-    # def test_GetUser(self):
+    # def test_UserChange(self):
     #
     #     headers = {"content-type": "application/json"}
     #
-    #     # 成功案例
-    #     json_data1 = {'userid': '1'}
-    #     r1 = requests.post(self.GetUser_url, json=json_data1, headers=headers)
+    #     #成功案例
+    #     json_data1 = {'userid': '1', 'username': 'cyhcyh', 'passwd': '123123'}
+    #     r1 = requests.post(self.UserChange_url, json=json_data1, headers=headers)
     #     result1 = r1.json()
-    #     print(result1)
     #     self.assertEqual(result1['ret'], 0)
+    #
+    #     # 失败案例
+    #     json_data2 = {'userid': '111', 'username': 'cyhcyh', 'passwd': '123123'}
+    #     r2 = requests.post(self.UserChange_url, json=json_data2, headers=headers)
+    #     result2 = r2.json()
+    #     self.assertEqual(result2['ret'], 1)
+
+    #获取用户详情测试(用于找回密码)
+    def test_GetUser(self):
+
+        headers = {"content-type": "application/json"}
+
+        # 成功案例
+        json_data1 = {'userid': '1'}
+        r1 = requests.post(self.GetUser_url, json=json_data1, headers=headers)
+        result1 = r1.json()
+        print(result1)
+        self.assertEqual(result1['ret'], 0)
 
         # 失败案例
         # json_data2 = {'userid': '111'}
