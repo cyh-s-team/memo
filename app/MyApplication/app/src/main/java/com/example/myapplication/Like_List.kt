b4ef987f4e09622ca8bec60a5b9577a57fe54d46
jumpshotsniper <1057152966@qq.com>
2022-06-07 21:22:15 +0800

    按日期搜索和日记提醒功能实现(前端)

--- app/MyApplication/app/src/main/java/com/example/myapplication/Like_List.kt
+++
@@ -1,3 +0,0 @@
-package com.example.myapplication.PepareClass
-
-class Like_List(val imageID:Int,val name:String,val time:String,val imageID1:Int)
--- app/MyApplication/app/src/main/java/com/example/myapplication/NewFollow_List.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.PepareClass
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class NewFollow_List(val imageID:Int,val name:String,val signature:String)
